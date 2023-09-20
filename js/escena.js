795cb9285588483992480c130b1f297298b93259
Ariel Coto <ariel96cs@gmail.com>
2023-09-20 19:15:38 +0200

    Added gltf models

--- js/escena.js
+++ js/escena.js
@@ -41,26 +41,73 @@ function init(){
     // Camera
     camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,
                                             0.1,1000);
-    camera.position.set(0.5,2,7);
+    camera.position.set(0.8,2,7);
     camera.lookAt(0,1,0);
 
 }
 
 function loadScene(){
     // se crea un mesh
-     const material = new THREE.MeshBasicMaterial({color:'yellow',wireframe: false});
+     const material = new THREE.MeshBasicMaterial({color:'yellow',wireframe: true});
     
      const geoCubo = new THREE.BoxGeometry(2,2,2);
+
+     const geoEsfera = new THREE.SphereGeometry(1,20,20);
     
     // se crea la malla
      const cubo = new THREE.Mesh(geoCubo,material);
+     const sphere = new THREE.Mesh(geoEsfera,material);
 
-     scene.add(cubo);
+     // otro objeto
+     esferaCubo = new THREE.Object3D();
+
+     //transformaciones
+     cubo.position.set(1,0,0);
+     sphere.position.set(-1,0,0);
+
+    esferaCubo.add(cubo);
+    esferaCubo.add(sphere);
+
+    esferaCubo.position.set(1,1,0);
+
+    scene.background = new THREE.Color(0.7,0.6,0.9);
+    scene.add(esferaCubo);
+     scene.add(new THREE.AxisHelper(2));
+
+     //suelo
+
+     const suelo = new THREE.Mesh(new THREE.PlaneGeometry(10,10,10,10),material);
+
+     // rotamos el suelo 90 grados
+     suelo.rotation.x = -Math.PI/2;
+
+     scene.add(suelo);
+
+     // importar un modelo json
+     const loader = new THREE.ObjectLoader();
+     loader.load('models/soldado/soldado.json',
+        function (obj){
+            cubo.add(obj);
+            obj.position.set(0,1,0);
+     })
+     
+     // importar modelo en gltf
+     const gltfLoader = new GLTFLoader();
+     gltfLoader.load('./models/extra/scene.gltf',function (gltf){
+        gltf.scene.position.y = 1;
+        gltf.scene.rotation.y = -Math.PI/2;
+        gltf.scene.scale.set(0.01,0.01,0.01);
+        sphere.add(gltf.scene);
+     });
+
+
+    
 
 }
 
 function update(){
-
+    angle+=0.01;
+    esferaCubo.rotation.y = angle;
 }
 
 function render(){
