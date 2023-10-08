c854c09e5bc357289e88552880b550b70d88fdb8
Ariel Coto <ariel96cs@gmail.com>
2023-10-08 18:08:53 +0200

    Moved for practica4 robot creation to a robot class

--- GPC/js/practica4.js
+++ GPC/js/practica4.js
@@ -1,27 +1,21 @@
 /**
- * Brazo robot seminario 4
+ * Brazo robot seminario 3
  * 
  * @author ariel96cs@gmail.com
  */
 
-/* TODO
 
-    1. Giro de la base , se gira todo el objeto
-    2. Desplazar el robot con las flechas del teclado
-    3. Giro del brazo.
-
-*/
 // Modulos necesarios
 import * as THREE from "../lib/three.module.js"
 import {OrbitControls} from "../lib/OrbitControls.module.js"
-import {crearPinza} from "./pinzas.js"
+import {Robot} from "./robot.js"
 
 // Variables de consenso
 let renderer, scene, camera;
 
 // Camaras adicionales
 let miniaturaCamera;
-const L = 8;
+const L = 4;
 
 // Controlador de camera
 let cameraControls;
@@ -132,7 +126,6 @@ function loadScene(){
     const groundGeometry = new THREE.PlaneGeometry(1000, 1000,50,50); // Ancho y largo del suelo
 
     // Crear un material para el suelo
-
     const groundMaterial = new THREE.MeshNormalMaterial({wireframe: false, flatShading: true});// Color del suelo
 
     // Crear una malla para el suelo
@@ -140,125 +133,16 @@ function loadScene(){
     groundMesh.rotation.x = -Math.PI / 2; // Rotar el suelo para que sea horizontal en el plano XZ
     scene.add(groundMesh);
 
-    // Crear el brazo del robot
-    const robot = new THREE.Object3D();
-    const brazo = new THREE.Object3D();
-    const antebrazo = new THREE.Object3D();
-    const mano = new THREE.Object3D();
-
-    // Se crea el material del robot
-    const robotMaterial = new THREE.MeshNormalMaterial({wireframe: false, flatShading: true}); //new THREE.MeshBasicMaterial({ color: 0x0000ff,wireframe: false }); // Color del robot
-    
-    // Crear la base cilíndrica del brazo del robot
-    const baseGeometry = new THREE.CylinderGeometry(0.5, 0.5, 0.15, 32); // Radio superior, radio inferior, altura, número de caras
-    const baseMesh = new THREE.Mesh(baseGeometry, robotMaterial);
-    baseMesh.position.y = 0.1; // Levantar la base 0.1 unidades para que no esté enterrada en el suelo
-
-    // El brazo de robot está formado por 4 partes: eje, esparrago, rotula y antebrazo
-    // Se crea el eje
-    const ejeGeometry = new THREE.CylinderGeometry(0.2, 0.2, 0.18, 32); // Radio superior, radio inferior, altura, número de caras
-    const ejeMesh = new THREE.Mesh(ejeGeometry, robotMaterial);
-    ejeMesh.position.y = 0.25; // Levantar el eje 0.6 unidades para que esté sobre la base
-    // rotar eje 90 grados sobre el eje x para que este en vertical
-    ejeMesh.rotation.x = Math.PI / 2;
-
-    // Se crea el esparrago
-    const esparragoGeometry = new THREE.BoxGeometry(0.18, 1.2, 0.12); // Ancho, alto, profundidad
-    const esparragoMesh = new THREE.Mesh(esparragoGeometry, robotMaterial);
-    esparragoMesh.position.y = 0.8; // Levantar el espárrago 1.1 unidades para que esté sobre el eje
-
-    // Se crea la rótula
-    const rotulaGeometry = new THREE.SphereGeometry(0.2, 32, 32); // Radio, número de caras en ancho y alto    
-    const rotulaMesh = new THREE.Mesh(rotulaGeometry, robotMaterial);
-    rotulaMesh.position.y = 1.4; // Levantar la rótula 1.2 unidades para que esté sobre el espárrago
-
-    // Se crea el antebrazo
-    // Un atebrazo está formado por 3 partes: disco, nervios y mano
-    // Se crea el disco
-    const discoGeometry = new THREE.CylinderGeometry(0.22, 0.22, 0.06, 32); // Radio superior, radio inferior, altura, número de caras
-    const discoMesh = new THREE.Mesh(discoGeometry, robotMaterial);
-    // Levantar el disco para que esté centrado sobre la rotula
-    discoMesh.position.y = 1.4;
-
-    // Se crean los 4 nervios del antebrazo
-    const numNervios = 4;
-    const radiusPrisms = 0.1; // Distancia desde el centro del cilindro
-    const nervioHeight = 0.8; // Altura de los prismas
-    const nervioWidth = 0.04; // Ancho de los nervios
-
-    const nervioGeometry = new THREE.BoxGeometry(nervioWidth, nervioHeight, nervioWidth); // Ancho, alto, profundidad
-    // Se añaden los nervios al antebrazo
-    for (let i = 0; i < numNervios; i++) {
-        const angle = (Math.PI * 2 * i) / numNervios; // Ángulo igual entre los prismas
-        const x = Math.cos(angle) * radiusPrisms;
-        const z = Math.sin(angle) * radiusPrisms;
-
-        const nervio = new THREE.Mesh(nervioGeometry, robotMaterial);
-
-        nervio.position.set(x, 1.4+nervioHeight/2, z);
-        antebrazo.add(nervio);
-    }
-
-
-    // Se crea la mano, que está formada por dos pinzas sobre un cilindro
-    // se crea el material de la pinza
-    const pinzaIzq = crearPinza(robotMaterial);
-    const pinzaDer = crearPinza(robotMaterial);
-    const cilindroGeometry = new THREE.CylinderGeometry(0.15, 0.15, 0.4, 32); // Radio superior, radio inferior, altura, número de caras
-    const cilindroMesh = new THREE.Mesh(cilindroGeometry, robotMaterial);
-    // Levantar el cilindro para que esté sobre los nervios, los nervios deben entrar en el cilindro
-    cilindroMesh.position.y = 2.3;
-    // Rotar el cilindro 90 grados sobre el eje X para que esté en vertical
-    cilindroMesh.rotation.x = Math.PI / 2;
-    
-    // Levantar la pinza
-    pinzaIzq.position.y = 2.2;
-    pinzaDer.position.y = 2.2;
-    // Separar las pinzas
-    pinzaIzq.position.z = -0.1;
-    pinzaDer.position.z = 0.1;
-
-    // Separar las pinzas
-    pinzaIzq.position.z = -0.1;
-    pinzaDer.position.z = 0.1;
-
-    pinzaDer.scale.z = -1; // Invertir la pinza derecha para que ambas pinzas estén enfrentadas
-
-    // Se añade la pinza izquierda a la mano
-    mano.add(pinzaIzq);
-    // // Se añade la pinza derecha a la mano
-    mano.add(pinzaDer);
-    // Se añade el cilindro a la mano
-    mano.add(cilindroMesh);
-
-    // Se  crea el grafo de escena en este orden, robot -> base -> brazo -> eje esparrago rotula antebrazo -> disco nervios mano
-    // Se añade la base al robot    
-    robot.add(baseMesh);
-    // Se añade el brazo al robot
-   
-    robot.add(brazo);
-    // Se añade el eje al brazo
-    brazo.add(ejeMesh);
-    // Se añade el espárrago al brazo
-    brazo.add(esparragoMesh);
-    // Se añade la rótula al brazo
-    brazo.add(rotulaMesh);
-    // Se añade el antebrazo al brazo
-   
-    brazo.add(antebrazo);
-    // Se añade el disco al antebrazo
-    antebrazo.add(discoMesh);
-    
-    
-    // Se añade la mano al antebrazo
-    antebrazo.add(mano);
-    
-    const scale = 3;
-    robot.scale.set(scale,scale,scale);
+    // Crear el robot
+    const robot = new Robot();
+    robot.setS
     //se añade el robot a la escena
     scene.add(robot);
 
-    robot.position.set(robotX,robotY,robotZ);
+    // Posicionar el robot
+    robot.setPosition(robotX,robotY,robotZ);
+    robot.addToScene(scene);
+
     scene.add(new THREE.AxesHelper(1000));
 }
 
@@ -283,7 +167,6 @@ function render(){
 
     renderer.setViewport(0,0,window.innerWidth,window.innerHeight);
     renderer.render(scene,camera);
-
 
 }
 
---
+++ GPC/js/robot.js
@@ -0,0 +1,271 @@
+/**
+ * Robot class object
+ * 
+ * author: ariel96cs
+ */
+
+// Modulos necesarios
+import * as THREE from "../lib/three.module.js"
+
+export {Robot};
+
+class Robot extends THREE.Object3D{ 
+    constructor(){
+        super();
+        // crear el material del robot
+        this.robotMaterial = new THREE.MeshNormalMaterial({wireframe: false, flatShading: true}); //new THREE.MeshBasicMaterial({ color: 0x0000ff,wireframe: false }); // Color del robot
+
+        this.generateRobotGraph();
+        this.scaleRobot();
+    }
+
+    generateRobotGraph(){
+        const eje = this.createEje();
+        const esparrago = this.createEsparrago();
+        const rotula = this.createrotula();
+        const disco = this.createDisco();
+        const nervios = this.createNervios();
+        const mano = this.createHand();
+
+        // Se crea el antebrazo, que está formado por el disco, los nervios y la mano
+        const antebrazo = new THREE.Object3D();
+        // Se añaden los nervios al antebrazo
+        nervios.forEach((nervio) => {
+            antebrazo.add(nervio);
+        });
+        // Se añade la mano al antebrazo
+        antebrazo.add(mano);
+        // Se añade el disco al antebrazo
+        antebrazo.add(disco);
+
+        // Se crea el brazo, que está formado por el eje, el espárrago, la rótula y el antebrazo
+        const brazo = new THREE.Object3D();
+        // Se añade el eje al brazo
+        brazo.add(eje);
+        // Se añade el espárrago al brazo
+        brazo.add(esparrago);
+        // Se añade la rótula al brazo
+        brazo.add(rotula);
+        // Se añade el antebrazo al brazo
+        brazo.add(antebrazo);
+
+        // Se crea la base del robot
+        const base = this.createBase();
+
+        // // Se crea el robot, que está formado por la base y el brazo
+        // const robot = new THREE.Object3D();
+        // // Se añade la base al robot
+        // robot.add(base);
+        // // Se añade el brazo al robot
+        // robot.add(brazo);
+
+        this.add(base);
+        this.add(brazo);
+
+        return this;
+    }
+
+    // scale robot
+    scaleRobot(scale=3){
+        this.scale.set(scale,scale,scale);
+    }
+
+    // crear la base del robot
+    createBase(){
+        // Crear la base cilíndrica del brazo del robot
+        const baseGeometry = new THREE.CylinderGeometry(0.5, 0.5, 0.15, 32); // Radio superior, radio inferior, altura, número de caras
+        const baseMesh = new THREE.Mesh(baseGeometry, this.robotMaterial);
+        baseMesh.position.y = 0.1; // Levantar la base 0.1 unidades para que no esté enterrada en el suelo
+
+        return baseMesh;
+    }
+
+    createEje(){
+        // Se crea el eje
+        const ejeGeometry = new THREE.CylinderGeometry(0.2, 0.2, 0.18, 32); // Radio superior, radio inferior, altura, número de caras
+        const ejeMesh = new THREE.Mesh(ejeGeometry, this.robotMaterial);
+        ejeMesh.position.y = 0.25; // Levantar el eje 0.6 unidades para que esté sobre la base
+        // rotar eje 90 grados sobre el eje x para que este en vertical
+        ejeMesh.rotation.x = Math.PI / 2;
+        return ejeMesh;
+    }
+
+    createEsparrago(){
+            // Se crea el esparrago
+    const esparragoGeometry = new THREE.BoxGeometry(0.18, 1.2, 0.12); // Ancho, alto, profundidad
+    const esparragoMesh = new THREE.Mesh(esparragoGeometry, this.robotMaterial);
+    esparragoMesh.position.y = 0.8; // Levantar el espárrago 1.1 unidades para que esté sobre el eje
+    return esparragoMesh;
+    }
+
+    createrotula(){
+            // Se crea la rótula
+    const rotulaGeometry = new THREE.SphereGeometry(0.2, 32, 32); // Radio, número de caras en ancho y alto    
+    const rotulaMesh = new THREE.Mesh(rotulaGeometry, this.robotMaterial);
+    rotulaMesh.position.y = 1.4; // Levantar la rótula 1.2 unidades para que esté sobre el espárrago
+    return rotulaMesh;
+    }
+
+    createDisco(){
+        const discoGeometry = new THREE.CylinderGeometry(0.22, 0.22, 0.06, 32); // Radio superior, radio inferior, altura, número de caras
+        const discoMesh = new THREE.Mesh(discoGeometry, this.robotMaterial);
+        // Levantar el disco para que esté centrado sobre la rotula
+        discoMesh.position.y = 1.4;
+        return discoMesh;
+    }
+
+    // Crea la lista de nervios para añadir al antebrazo
+    createNervios(){
+    // Se crean los 4 nervios del antebrazo
+    const numNervios = 4;
+    const radiusPrisms = 0.1; // Distancia desde el centro del cilindro
+    const nervioHeight = 0.8; // Altura de los prismas
+    const nervioWidth = 0.04; // Ancho de los nervios
+    
+
+    // se instancia la lista de nervios
+    const nervios = [];
+
+    const nervioGeometry = new THREE.BoxGeometry(nervioWidth, nervioHeight, nervioWidth); // Ancho, alto, profundidad
+    // Se añaden los nervios al antebrazo
+    for (let i = 0; i < numNervios; i++) {
+        const angle = (Math.PI * 2 * i) / numNervios; // Ángulo igual entre los prismas
+        const x = Math.cos(angle) * radiusPrisms;
+        const z = Math.sin(angle) * radiusPrisms;
+
+        const nervio = new THREE.Mesh(nervioGeometry, this.robotMaterial);
+
+        nervio.position.set(x, 1.4+nervioHeight/2, z);
+        // antebrazo.add(nervio);
+        nervios.push(nervio);
+    }
+    return nervios;
+
+    }
+
+    createHand(){
+    // Se crea la mano, que está formada por dos pinzas sobre un cilindro
+    const mano = new THREE.Object3D();
+    // se crea el material de la pinza
+    // const pinzasMaterial = new THREE.MeshNormalMaterial({wireframe: fa, flatShading: true}); //new THREE.MeshBasicMaterial({ color: 0x0000ff,wireframe: false }); // Color del robot
+    const pinzaIzq = this.createPinza(this.robotMaterial);
+    const pinzaDer = this.createPinza(this.robotMaterial);
+    const cilindroGeometry = new THREE.CylinderGeometry(0.15, 0.15, 0.4, 32); // Radio superior, radio inferior, altura, número de caras
+    const cilindroMesh = new THREE.Mesh(cilindroGeometry, this.robotMaterial);
+    // Levantar el cilindro para que esté sobre los nervios, los nervios deben entrar en el cilindro
+    cilindroMesh.position.y = 2.3;
+    // Rotar el cilindro 90 grados sobre el eje X para que esté en vertical
+    cilindroMesh.rotation.x = Math.PI / 2;
+    
+    // Levantar la pinza
+    pinzaIzq.position.y = 2.2;
+    pinzaDer.position.y = 2.2;
+    // Separar las pinzas
+    pinzaIzq.position.z = -0.1;
+    pinzaDer.position.z = 0.1;
+
+    // trasladar las pinzas hacia adelante
+    pinzaIzq.position.x = 0.1;
+    pinzaDer.position.x = 0.1;
+
+    pinzaDer.scale.z = -1; // Invertir la pinza derecha para que ambas pinzas estén enfrentadas
+
+    // Se añade la pinza izquierda a la mano
+    mano.add(pinzaIzq);
+    // // Se añade la pinza derecha a la mano
+    mano.add(pinzaDer);
+    // Se añade el cilindro a la mano
+    mano.add(cilindroMesh);
+
+    return mano;
+    }
+
+    createPinza(){
+
+        const pinzasMaterial =this.robotMaterial;
+        // crear la palma de la pinza
+        const palma = new THREE.Mesh(new THREE.BoxGeometry(0.19,0.2,0.04),pinzasMaterial);
+        // setting position at the 0,0,0
+        palma.position.y = 0.1; 
+    
+        // crear el dedo de la pinza
+        // el dedo es un prisma que tiene bases rectangulares y caras laterales trapezoidales
+        const dedo = new THREE.BufferGeometry();
+        const vertices = new Float32Array([
+                        0,0,0, // 0
+                        0.04,0,0, // 1
+                        0,0,0.2, // 2
+                        0.04,0,0.2, // 3
+                        0,0.19,0.02, // 4
+                        0.02,0.19,0.02, // 5
+                        0.0,0.19,0.18, // 6
+                        0.02,0.19,0.18, // 7
+        ]);
+        const indices = new Uint16Array([
+            0,1,2, // lateral izquierdo
+            1,3,2, // lateral izquierdo
+            0,1,4, // base inferior
+            1,5,4, // base inferior
+            5,4,6, // lateral derecho
+            5,6,7, // lateral derecho
+            2,3,7, // base superior
+            2,7,6, // base superior
+            1,7,3, // cara frontal
+            1,5,7, // cara frontal
+            0,6,2, // cara trasera
+            0,4,6, // cara trasera
+        ]);
+        
+        dedo.setIndex(new THREE.BufferAttribute(indices,1));
+        dedo.setAttribute('position',new THREE.BufferAttribute(vertices,3));
+        const dedoMesh = new THREE.Mesh(dedo,pinzasMaterial);
+    
+        // rotar 90 grados sobre el eje y 
+        dedoMesh.rotateY(Math.PI/2);
+        dedoMesh.rotateX(Math.PI/2);
+    
+        // levantar el dedo 0.1 unidades para que esté centrado sobre la palma
+        dedoMesh.position.y = palma.position.y*2;
+        dedoMesh.position.x = 0.19/2;
+        dedoMesh.position.z = 0.04/2;
+    
+        // crear la pinza
+        const pinza = new THREE.Object3D();
+        pinza.add(palma);
+        pinza.add(dedoMesh);
+    
+        pinzasMaterial.side = THREE.DoubleSide;
+    
+        return pinza;
+    }
+
+    // añadir robot a la escena
+    addToScene(scene){
+        scene.add(this);
+    }
+
+    // setear posicion del robot
+    setPosition(x,y,z){
+        this.position.set(x,y,z);
+    }
+
+    // rotar la base del robot
+    rotateBase(angle){
+        this.rotation.y += angle;
+    }
+
+    // rotar el brazo del robot
+    rotateArm(angle){
+        this.children[1].rotation.z += angle;
+    }
+
+    // rotar el antebrazo del robot
+    rotateForearm(angle){
+        this.children[2].rotation.z += angle;
+    }
+
+    // rotar la mano del robot
+    rotateHand(angle){
+        this.children[3].rotation.y += angle;
+    }
+}
